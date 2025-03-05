ba09b9e192053206e2e6b6b23d00fde19d5c7a07
ManBenit <manuel.e.benitez.m@gmail.com>
2025-03-05 07:35:58 -0600

    Python Tops updated

--- examples/fsm_myfsm/UVM_TB/Envmnt/RefMdl/RTLRef/fsm_myfsm/obj_dir/tb_rm_fsm_myfsm.d
+++
@@ -1,6 +0,0 @@
-tb_rm_fsm_myfsm.o: ../tb_rm_fsm_myfsm.cpp Vrm_MyFSM.h \
- /usr/local/share/verilator/include/verilated.h \
- /usr/local/share/verilator/include/verilatedos.h \
- /usr/local/share/verilator/include/verilated_config.h \
- /usr/local/share/verilator/include/verilated_types.h \
- /usr/local/share/verilator/include/verilated_funcs.h
--- examples/mux_2_a_4/Top_MUX.py
+++ examples/mux_2_a_4/Top_MUX.py
@@ -4,13 +4,17 @@
 
 import paths
 import cocotb
-import pyuvm
 from pyuvm import uvm_root
+from colorama import Fore
+import pyfiglet
+from utils import load_config
 
 from Test import Test
 
+CONFIG = load_config('config.json')
+
 @cocotb.test()
 async def main_test(dut):
-    """################## This is a UVMEnv verification project. ##################"""
+    print( Fore.BLUE+pyfiglet.figlet_format('UVMEnv')+Fore.RESET )
+    print( Fore.YELLOW+pyfiglet.figlet_format(CONFIG.dut_design.top_module)+Fore.RESET )
     await uvm_root().run_test("Test")
-
